2992ef1c1515da534593f529ac6b25329a05ba23
george <george.georgiev@hotmail.com>
2016-05-18 17:27:18 -0700

    my first go code

--- src/main.go
+++ src/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"pack"
 )
 
 func main() {
-	fmt.Printf("Hello %d World.", pack.Export())
+	pack.Publish()
 }
--- src/pack/pack.go
+++ src/pack/pack.go
@@ -1,5 +1,17 @@
 package pack
 
-func Export() int32 {
-	return 1
+import (
+	"github.com/nsqio/go-nsq"
+	"log"
+)
+
+func Publish() {
+	config := nsq.NewConfig()
+	w, _ := nsq.NewProducer("127.0.0.1:4150", config)
+
+	err := w.Publish("write_test", []byte("test"))
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+	w.Stop()
 }
